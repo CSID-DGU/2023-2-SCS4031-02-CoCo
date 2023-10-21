e6a77d194d3c89082c8cbf18b41aeb0d54c5026d
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-10-21 18:59:12 +0900

    [ADD] #3 - 게시글 에디터 설치 및 route에 장소 등록 url 추가

--- trizzle-front/src/route/index.tsx
+++ trizzle-front/src/route/index.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPlanPage from '../pages/UserPlan/AddPlan';
 import Home from '../pages/Home';
+import AddPlacePage from '../pages/UesrPlace/AddPlace';
+import PostEditor from '../pages/UesrPlace/PostEditor';
 
 
 const Router = () => {
@@ -9,6 +11,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id/plan/addPlan" element={<AddPlanPage />} />
+        <Route path="/post/places/add" element={<AddPlacePage />} />
       </Routes>
     </BrowserRouter>
   );
