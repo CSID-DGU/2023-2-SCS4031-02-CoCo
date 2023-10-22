841ec111974be036f755e4ae8c67466d12c76213
yurik1m <yuri.k.tech@gmail.com>
2023-10-22 15:34:23 +0900

    [ADD]일정 리스트 페이지 추가

---
+++ trizzle-front/src/shared/PlanListContainer/PlanListContainer.style.tsx
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div<{past:boolean}>`
+  width: 47%;
+  height: 7.5rem;
+  border-radius: 0.75rem;
+  background-color: #ffffff;
+  display: flex;
+  border: ${props => props.past ? "1px solid #BDBDBD" : "1px solid #747474"};
+  margin: 1rem 0; 
+  cursor: pointer;
+  &:hover {
+    background-color: #f6f6f6;
+  }
+`
+
+export const LeftContainer = styled.div<{past:boolean}>`
+  width: 10%;
+  height: 100%;
+  border-top-left-radius: 0.75rem;
+  border-bottom-left-radius: 0.75rem;
+  background-color: ${props => props.past ? "#BDBDBD" : "#F9E0E3"};
+`
+
+export const RightContainer = styled.div`
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 0.5rem;
+  border-top-right-radius: 0.75rem;
+  border-bottom-right-radius: 0.75rem;
+  gap: 0.5rem;
+`
+
+export const Title = styled.div<{past:boolean}>`
+  width: 100%;
+  height: auto;
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: ${props => props.past ? "#BDBDBD" : "#000000"};
+`
+
+export const Location = styled.div<{past:boolean}>`
+  width: 100%;
+  height: auto;
+  font-size: 0.8rem;
+  font-weight: 400;
+  color: ${props => props.past ? "#BDBDBD" : "#747474"};
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`
+export const Date = styled.div<{past:boolean}>`
+  width: 100%;
+  height: auto;
+  font-size: 0.8rem;
+  font-weight: 400;
+  color: ${props => props.past ? "#BDBDBD" : "#747474"};
+`
+
