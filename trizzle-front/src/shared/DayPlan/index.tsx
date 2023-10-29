3d4709b21bdbc8e40c731643f23fdfe1b2567ba4
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-10-29 17:22:22 +0900

    Feat-코드 오류 수정

--- trizzle-front/src/shared/DayPlan/index.tsx
+++ trizzle-front/src/shared/DayPlan/index.tsx
@@ -1,5 +1,4 @@
-import React,{useState} from "react";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import * as S from "./DayPlan.style";
 import DayPlace from "./DayPlace";
 import { DragDropContext, Draggable } from "react-beautiful-dnd";
@@ -7,13 +6,13 @@ import { DragDropContext, Draggable } from "react-beautiful-dnd";
 //Drag & Drop 가능하도록 수정 예정
 
 type DayPlanProps = {
-  onPlaceClick: (day:number) => void;
-  onKeywordClick: (day:number) => void;
+  onPlaceClick: (day: number) => void;
+  onKeywordClick: (day: number) => void;
   dayPlan: dayPlan;
   isPlan: boolean;
   isPost?: boolean;
   onPostClick?: () => void;
-  onDeleteClick: (place:any, day:number, index:number) => void;
+  onDeleteClick: (place: any, day: number, index: number) => void;
 }
 
 type dayPlan = {
@@ -24,16 +23,13 @@ type dayPlan = {
 const DayPlan: React.FC<DayPlanProps> = (props: DayPlanProps) => {
 
 
-  return(
+  return (
     <S.DayPlanContainer>
 
 
       <S.DayPlanTitle>{props.dayPlan.day}일차</S.DayPlanTitle>
       {props.dayPlan.placeList.length > 0 && props.dayPlan.placeList.map((place, index) => (
-        <DayPlace key={index} place={place} day={props.dayPlan.day} isPlan={props.isPlan} isPost={props.isPost} onPostClick={props.onPostClick} index={index} onDeleteClick={(place, day, index) => props.onDeleteClick(place, day, index)}/>
-      ))
-      }
-        <DayPlace key={index} place={place} day={props.dayPlan.day} isPlan={props.isPlan} isPost={props.isPost} onPostClick={props.onPostClick} index={index} onDeleteClick={(place, day, index) => props.onDeleteClick(place, day, index)}/>
+        <DayPlace key={index} place={place} day={props.dayPlan.day} isPlan={props.isPlan} isPost={props.isPost} onPostClick={props.onPostClick} index={index} onDeleteClick={(place, day, index) => props.onDeleteClick(place, day, index)} />
       ))
       }
       <S.PlusButtonContainer>
