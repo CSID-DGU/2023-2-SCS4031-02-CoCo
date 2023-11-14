8eec592298be4721587e4618f6cf015c7b923f78
yurik1m <yuri.k.tech@gmail.com>
2023-11-14 20:18:16 +0900

    [FEAT] JwtFilter 되돌리기

--- trizzle-backend/src/main/java/trizzle/trizzlebackend/configuration/JwtFilter.java
+++ trizzle-backend/src/main/java/trizzle/trizzlebackend/configuration/JwtFilter.java
@@ -5,18 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.el.parser.Token;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 import trizzle.trizzlebackend.Utils.JwtUtil;
-import trizzle.trizzlebackend.error.ErrorCode;
-import trizzle.trizzlebackend.error.ErrorResponse;
-import trizzle.trizzlebackend.error.TokenError;
 
 import java.io.IOException;
 import java.util.List;
@@ -40,12 +34,12 @@ public class JwtFilter extends OncePerRequestFilter {
 
             // cookie에 token 없을경우 block
             if (token == null) {
-                throw new TokenError("로그인이 필요합니다", ErrorCode.Unauthorized);
+                throw new ServletException("토큰이 존재하지 않습니다.");
             }
 
             // Token Expired 되었는지 여부
             if (JwtUtil.isExpired(token, secretKey)) {
-                throw new TokenError("다시 로그인해주세요", ErrorCode.Unauthorized);
+                throw new ServletException("토큰이 만료되었습니다.");
             }
 
             // userId token에서 꺼내기
@@ -56,14 +50,13 @@ public class JwtFilter extends OncePerRequestFilter {
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(accountId, null, List.of(new SimpleGrantedAuthority("USER")));
 
-            // 원래 동작 수행
-            filterChain.doFilter(request, response);
-        } catch (TokenError e) {
+            //detail
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } catch (Exception e) {
             log.error(e.getMessage());
-            ErrorResponse res = new ErrorResponse(e.getErrorCode());
-            res.setMessage(e.getMessage());
-            response.setStatus(e.getErrorCode().getStatus());
-            response.getWriter().write(res.toString());
         }
+
+        filterChain.doFilter(request, response);
     }
 }
