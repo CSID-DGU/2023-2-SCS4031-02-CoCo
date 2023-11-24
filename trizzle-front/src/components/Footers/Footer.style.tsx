468c721e1291cc4fc6e2ae623677260cb6da41f5
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-11-24 16:12:10 +0900

    [ADD] Footer 내용 추가

--- trizzle-front/src/components/Footers/Footer.style.tsx
+++ trizzle-front/src/components/Footers/Footer.style.tsx
@@ -1,8 +1,57 @@
 import styled from "@emotion/styled";
 
 export const Footer = styled.footer`
+  display: flex;
   width: 100%;
   height: 6rem;
-  display: flex;
   background-color: #ededed;
+`
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 1rem 5rem;
+  width: 100%;
+`
+export const HorizontalContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+export const VerticalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+export const MinText = styled.div`
+  padding: 0.2rem 1rem;
+  font-size: 0.7rem;
+  color: #949494;
+`
+
+export const MIddleText = styled.div`
+  margin: 0.2rem 0 0 1rem;
+  font-size: 0.8rem;
+  color: #949494;
+`
+export const LargeText = styled.div`
+  margin: 0.2rem 1rem;
+  font-size: 0.9rem;
+  color: #949494;
+`
+
+export const MIddleTextLink = styled.div`
+  margin: 0.2rem 0;
+  font-size: 0.8rem;
+  font-weight: 500;
+  cursor: pointer;
+`
+
+export const Sortation = styled.div`
+  margin: 0.2rem 0 0 1rem;
+  font-size: 0.8rem;
+  color: #949494;
 `
