b1640d60912c6e045265cbf31431ed62dad86080
yurik1m <yuri.k.tech@gmail.com>
2023-11-26 00:56:28 +0900

    [FEAT] 일정 리스트 수정

--- trizzle-front/src/shared/PlanListContainer/index.tsx
+++ trizzle-front/src/shared/PlanListContainer/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import * as S from "./PlanListContainer.style";
 import {BiMapPin} from "react-icons/bi";
-import Menu from "../../components/Menu";
 
 
 
@@ -14,14 +13,11 @@ type PlanListContainerProps = {
 
 
 export const PlanListContainer: React.FC<PlanListContainerProps> = (props: PlanListContainerProps) => {
-  const menuItem = [{content: "삭제", onClick:()=> props.onDeleteClick(props.plan.id), isDelete: true}];
-  // const [menuItems, setMenuItems] = useState<any[]>(menuItem);
 
 
   return (
         <S.Container past={props.past}>
           <S.LeftContainer past={props.past}/>
-            <Menu item={menuItem} />
           <S.RightContainer>
             <S.Title past={props.past}>{props.plan.planName}</S.Title>
             <S.Location past={props.past}>
