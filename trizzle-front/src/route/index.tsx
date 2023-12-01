9fa3ecd5fe72beb8b2c01b81a66f4ca3604a26bf
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2023-12-01 15:08:58 +0900

    [STYLE] build 오류 수정

--- trizzle-front/src/route/index.tsx
+++ trizzle-front/src/route/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPlanPage from '../pages/AddPlan/AddPlan';
 import Home from '../pages/Home';
 import AddPlacePage from '../pages/AddPlace/AddPlace';
