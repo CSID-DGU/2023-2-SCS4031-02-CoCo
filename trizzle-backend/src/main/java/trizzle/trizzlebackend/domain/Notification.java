ca70c2b8b2c0bfbcbde2ebf1d79a7ed1ea0bb25a
yurik1m <yuri.k.tech@gmail.com>
2023-12-03 15:30:51 +0900

    [ADD] #91 팔로우 알림 추가

--- trizzle-backend/src/main/java/trizzle/trizzlebackend/domain/Notification.java
+++ trizzle-backend/src/main/java/trizzle/trizzlebackend/domain/Notification.java
@@ -1,6 +1,7 @@
 package trizzle.trizzlebackend.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
