72e05eb24454c58de9eff5bcc4b2d2f73d4845d8
yurik1m <yuri.k.tech@gmail.com>
2023-12-07 23:53:30 +0900

    [FEAT] 전체 검색에서도 장소 이름 keyword 적용

--- trizzle-backend/src/main/java/trizzle/trizzlebackend/repository/ElasticReviewRepository.java
+++ trizzle-backend/src/main/java/trizzle/trizzlebackend/repository/ElasticReviewRepository.java
@@ -8,7 +8,7 @@ import trizzle.trizzlebackend.elasticSearch.ElasticsearchRepository;
 
 public interface ElasticReviewRepository extends ElasticsearchRepository<ElasticReview, Long> {
 
-    @Query("{\"bool\": {\"should\": [{\"match\": {\"reviewTitle\": \"?0\"}}, {\"match\": {\"reviewContent\": \"?0\"}}]}}")
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"reviewTitle\": \"?0\"}}, {\"match\": {\"reviewContent\": \"?0\"}}, {\"match\": {\"place.placeName\": \"?0\"}}]}}")
     Page<ElasticReview> searchByReviewTitleOrReviewContentText(String keyword, Pageable pageable);
 
     @Query("{\"bool\": {\"must\": [" +
